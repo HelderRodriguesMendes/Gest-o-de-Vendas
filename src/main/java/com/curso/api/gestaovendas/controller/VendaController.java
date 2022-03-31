97cc2d7b5120d83c3176100d709bce771f1aa178
helde <helderrodrigues37@gmail.com>
2022-03-31 00:11:37 -0300

    Refatorando controller de Venda

--- src/main/java/com/curso/api/gestaovendas/controller/VendaController.java
+++ src/main/java/com/curso/api/gestaovendas/controller/VendaController.java
@@ -10,6 +10,7 @@ import com.curso.api.gestaovendas.service.ItemVendaService;
 import com.curso.api.gestaovendas.service.VendaService;
 import com.curso.api.gestaovendas.util.Convert;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -33,18 +34,21 @@ public class VendaController {
     Convert convert = new Convert();
     List<Venda> vendas = new ArrayList<>();
 
+    @ApiOperation(value = "Salvar Venda", nickname = "salvarVenda")
     @PostMapping("/cliente/{idCliente}")
     public ResponseEntity<VendaResponseDTO> salvar(@PathVariable Long idCliente, @Valid @RequestBody VendaRequestDTO vendaRequestDTO){
         Venda vendaSalva = vendaService.salvar(idCliente, vendaRequestDTO);
         return new ResponseEntity<>(convert.toVendaResponseDTO(vendaSalva, vendaRequestDTO.getItemVendaRequestDTOS(), itemVendaService), HttpStatus.CREATED);
     }
 
+    @ApiOperation(value = "Bucar todas as vendas", nickname = "getAllVendas")
     @GetMapping("/getAll")
     public ResponseEntity<List<VendaResponseDTO>> getAllVendas(){
         List<Venda> vendas = vendaService.getAllVendas();
         return new ResponseEntity<>(convert.toListVendaResponseDTO(vendas, itemVendaService), HttpStatus.OK);
     }
 
+    @ApiOperation(value = "Bucar vendas de um cliente", nickname = "getVendaByIdCliente")
     @GetMapping("/vendaByIdCliente/{id}")
     public ResponseEntity<ClienteVendaResponseDTO> getVendaByIdCliente(@PathVariable Long id){
         List<Venda> vendas = vendaService.findVendaByIdCliente(id);
@@ -53,18 +57,21 @@ public class VendaController {
             convert.toListVendaResponseDTO(vendas, itemVendaService)), HttpStatus.OK);
     }
 
+    @ApiOperation(value = "Bucar uma venda por ID", nickname = "getVendaById")
     @GetMapping("/{id}")
     public ResponseEntity<VendaResponseDTO> getVendaById(@PathVariable Long id){
         Venda venda = vendaService.getVendaById(id);
         return new ResponseEntity<>(convert.toVendaResponseDTO(venda, itemVendaService), HttpStatus.OK);
     }
 
+    @ApiOperation(value = "Bucar Vendas por uma data", nickname = "getVendaByData")
     @GetMapping("/getVendaByData")
     public ResponseEntity<List<VendaResponseDTO>> getVendaByData(@RequestParam String data){
         vendas = vendaService.getVendaByData(data);
         return new ResponseEntity<>(convert.toListVendaResponseDTO(vendas, itemVendaService), HttpStatus.OK);
     }
 
+    @ApiOperation(value = "Atualizar venda", nickname = "atualizarVenda")
     @PutMapping("/{idVenda}/cliente/{idCliente}")
     public ResponseEntity<VendaResponseDTO> atualizar(@PathVariable Long idVenda, @PathVariable Long idCliente, @RequestBody VendaRequestDTO vendaRequestDTO){
         List<ItemVenda> itemVendasAtualizadas = itemVendaService.atualizar(vendaRequestDTO.getItemVendaRequestDTOS(), idVenda);
@@ -72,6 +79,7 @@ public class VendaController {
         return new ResponseEntity<>(convert.toVendaResponseDTO(itemVendasAtualizadas, vendaAtualizada), HttpStatus.OK);
     }
 
+    @ApiOperation(value = "Deletar uma venda", nickname = "deletarVenda")
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deletar(@PathVariable Long id){
